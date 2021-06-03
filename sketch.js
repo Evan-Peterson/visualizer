43d69f7ba95701dd148e445730f3829285ee4013
Evan-Peterson <4epeterson99@gmail.com>
2021-06-03 15:21:53 -0500

    added menu bar and some code documentation

--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
+
+// Initial canvas setup
 function setup() {
+    // Subtracts 25 from windowHeight to try and get rid of scroll bar
     createCanvas(windowWidth, windowHeight - 25);
-  }
-  
-  function draw() {
+}
+
+// Continually looping function that draws things to the canvas
+function draw() {
     //background(0, 100, 200);
-  }
+}
   
-  function windowResized() {
+// If the window is resized, resize the canvas to the new window size
+function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-  }
+}
