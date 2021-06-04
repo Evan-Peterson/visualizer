c2dedec696fcddaa52d91a10505a0964b805144e
Evan-Peterson <4epeterson99@gmail.com>
2021-06-04 15:06:24 -0500

    added goal and target information to node class

--- Node.js
+++ Node.js
@@ -1,13 +1,27 @@
 
 
-function Node(x,y,w) {
+function Node(x, y, w, isGoal) {
 
+    // x and y coordinates for the node
     this.x = x;
     this.y = y;
+
+    // Width to of the node
     this.w = w;
+
+    // True if the node is the goal node or not
+    this.isGoal = false;
 
     this.display = function() {
         fill(51);
         rect(this.x, this.y, this.w,this.w);
+    };
+
+    this.isTarget = function() {
+        return isGoal;
+    };
+
+    this.setGoal = function(isGoal) {
+        this.isGoal = isGoal;
     };
 }
