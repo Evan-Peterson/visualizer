47d8c46ad18366208d1cdae0ce672b0927e6642e
Evan-Peterson <4epeterson99@gmail.com>
2021-06-04 15:06:56 -0500

    fixed node constructor in sketch.js

--- board.js
+++ board.js
@@ -12,7 +12,7 @@ function board() {
     this.init = function() {
         for(var i = 0;i < this.columns;i++) {
             for(var j = 0;j < this.rows;j++) {
-                this.board[i][j] = new Node(i*this.w,j*this.w, this.w);
+                this.board[i][j] = new Node(i*this.w,j*this.w, this.w, false);
             }
         }
 
